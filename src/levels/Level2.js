a5b3120ba4bc966740bed3199b94cdd54156bbf8
Marcelo Autran <mrautran@gmail.com>
2018-10-13 15:38:45 -0700

    Added level 2 state modification functions and win comparison

--- src/levels/Level2.js
+++ src/levels/Level2.js
@@ -9,48 +9,74 @@ import './styles/level-styles.css';
 import './styles/level2.css'
 import '../styles.css'
 
+function getRgbString(stateObj) {
+  const { red, green, blue } = stateObj;
+  return `rgb(${red}, ${green}, ${blue})`
+}
+
+@inject('RoutingStore')
+@observer
 export default class Level2 extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      blockColor: 'rgb(255, 255, 255)'
+      red: 0,
+      green: 0,
+      blue: 0
     }
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidUpdate() {
-    if (winStateFunctions[1](this.state)) {
+    let state = { blockColor: getRgbString(this.state)}
+    if (winStateFunctions[1](state)) {
       this.props.RoutingStore.push('/')
     }
   }
 
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
   render() {
+    const blockColor = getRgbString(this.state);
     return (
         <div className={"level-container center-contents"}>
-          <div>
-            <div>
+          <div className={"level-2-stage"}>
+            <div className={"slider-container"}>
               <input
                 max="255"
                 min="0"
                 step="51"
                 type="range"
+                name="red"
+                onChange={this.handleChange}
+                value={this.state.red}
                 ></input>
               <input
                 max="255"
                 min="0"
                 step="51"
                 type="range"
+                name="green"
+                onChange={this.handleChange}
+                value={this.state.green}
                 ></input>
               <input
                 max="255"
                 min="0"
                 step="51"
                 type="range"
+                name="blue"
+                onChange={this.handleChange}
+                value={this.state.blue}
                 ></input>
             </div>
             <div
               className={"goal-block"}
-              style={{backgroundColor: this.state.blockColor}}>
+              style={{backgroundColor: blockColor}}>
             </div>
             <div>
               <div className={"goal-block"}>
--- src/levels/win-states.js
+++ src/levels/win-states.js
@@ -2,12 +2,13 @@ function level1WinState(state) {
   return (state.email.length > 5 && state.password.length > 5);
 }
 
-function level2WinState(stat) {
+function level2WinState(state) {
   return (state.blockColor === 'rgb(153, 102, 255)');
 }
 
 const winStateFunctions = [
-  level1WinState
+  level1WinState,
+  level2WinState
 ]
 
 export default winStateFunctions;
