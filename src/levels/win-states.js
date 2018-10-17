5729f23ba53dad0b74a7a68fe2271116456b2057
Marcelo Autran <mrautran@gmail.com>
2018-10-16 18:59:59 -0700

    Added regex to validate email for level 1 win state

--- src/levels/win-states.js
+++ src/levels/win-states.js
@@ -1,5 +1,7 @@
+const emailRe = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 function level1WinState(state) {
-  return (state.email.length > 5 && state.password.length > 5);
+  return (emailRe.test(state.email) && state.password.length > 5 );
 }
 
 function level2WinState(state) {
